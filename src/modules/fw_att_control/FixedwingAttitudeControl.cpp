d011aa7d47262042e41b28146e6f8ffd0f971355
Roman <bapstroman@gmail.com>
2018-04-05 16:00:35 +0200

    FixedWingAttitudeControl: small gain scheduling hack for airspeed less tailsitters
    Signed-off-by: Roman <bapstroman@gmail.com>

--- src/modules/fw_att_control/FixedwingAttitudeControl.cpp
+++ src/modules/fw_att_control/FixedwingAttitudeControl.cpp
@@ -602,6 +602,10 @@ void FixedwingAttitudeControl::run()
 					perf_count(_nonfinite_input_perf);
 				}
 
+				if (_vehicle_status.in_transition_mode || !_vehicle_status.is_rotary_wing) {
+					airspeed = _parameters.airspeed_max;
+				}
+
 				/*
 				 * For scaling our actuators using anything less than the min (close to stall)
 				 * speed doesn't make any sense - its the strongest reasonable deflection we
